9d680263618a303992e00e769bcb52ed1281ebbb
crash2bash <sachagurov@gmail.com>
2020-04-22 13:40:26 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2864373d6bc61c7e48f9e43b143db318.js"
+  "./precache-manifest.0b8e617b446139487662f922d3311fb9.js"
 );
 
 self.addEventListener('message', (event) => {
